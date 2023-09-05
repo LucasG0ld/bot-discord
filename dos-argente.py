0f7445f878d80c5b700f4235f873c26b0ae248d5
LucasG0ld <72749416+LucasG0ld@users.noreply.github.com>
2023-09-05 12:43:40 +0200

    Bot updates V. 1.0.1

--- dos-argente.py
+++ dos-argente.py
@@ -23,14 +23,35 @@ role_mappings = {
     "💀": "Escape the backrooms",
     "🔫": "Valorant",
     "⭐": "Summoners War",
-    "🌍": "Cube World",
     "💂": "World of Tanks",
     "🌳": "Minecraft",
-    "🥇": "Teamfight Tactics"
+    "🥇": "Teamfight Tactics",
+    "🚀": "War Thunder"
+}
+
+role_bases = {
+    "LES NOUVEAUX": None,
+    "LES HABITUÉS": None,
+    "LES COPAINS": None
 }
 
 # Dictionary to store users who have reacted
 users_with_reactions = {}
+
+async def assign_new_member_role(member):
+    has_base_role = False
+    for role_name in role_bases:
+        role = discord.utils.get(member.roles, name=role_name)
+        if role:
+            has_base_role = True
+            break
+    
+    if not has_base_role:
+        for emoji in role_mappings:
+            if discord.utils.get(member.roles, name=role_mappings[emoji]):
+                role = discord.utils.get(member.guild.roles, name="LES NOUVEAUX")
+                if role:
+                    await member.add_roles(role)
 
 @bot.event
 async def on_ready():
@@ -40,7 +61,7 @@ async def on_ready():
 async def on_member_join(member):
     print(f"New member joined: {member.name}")
     channel = bot.get_channel(WELCOME_CHANNEL_ID)
-    message = await channel.send(f"Bienvenue, {member.mention}! Réagis avec les emojis en fonction des jeux auxquels tu joues :")
+    message = await channel.send(f"Bienvenue, {member.mention}! Réagis avec les emojis en fonction des jeux auxquels tu joues :\n🚿 : League of Legends\n🚖 : Rocket League\n💀 : Escape the backrooms\n🔫 : Valorant\n⭐ : Summoners War\n💂 : World of Tanks\n🌳 : Minecraft\n🥇 : Teamfight Tactics\n🚀 : War Thunder")
     for emoji in role_mappings:
         await message.add_reaction(emoji)
     
@@ -64,6 +85,9 @@ async def on_raw_reaction_add(payload):
                     member = guild.get_member(payload.user_id)
                     if member.id == users_with_reactions[payload.message_id]:
                         await member.add_roles(role)
+                        
+                        # Assign the "LES NOUVEAUX" role as well
+                        await assign_new_member_role(member)
 
 @bot.event
 async def on_raw_reaction_remove(payload):
